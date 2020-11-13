d8caa495f35c1ee061fbb893960f14d0f1be42aa
adrian moreno <adrianmorenosal@gmail.com>
2020-11-12 20:19:06 -0500

    ole tengo un problema con app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
+import { APP_ROUTING} from './app.routes';
+
+
+// Servicios
+
+
+
+// componentes
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BodyComponent } from './components/body/body.componetn';
@@ -9,6 +19,9 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { CuentaComponent } from './components/cuenta/cuenta.component';
 import { IngresaComponent } from './components/ingresa/ingresa.component';
+import { Publica } from './component/publica.ts/publica.ts.component';
+import { Publica } from './components/publica.ts/publica.ts.component';
+import { PublicaComponent } from './components/publica/publica.component';
 
 @NgModule({
   declarations: [
@@ -20,9 +33,13 @@ import { IngresaComponent } from './components/ingresa/ingresa.component';
     HomeComponent,
     CuentaComponent,
     IngresaComponent,
+    Publica.Component,
+    Publica.TsComponent,
+    PublicaComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 
 import { RouterModule, Routes } from '@angular/router';
 import { IngresaComponent } from './components/ingresa/ingresa.component';
+import { CuentaComponent } from './components/cuenta/cuenta.component';
+import { HomeComponent } from './components/Home/Home.component';
 
 const APP_ROUTES: Routes = [
-{ path: 'Inicio', component: IngresaComponent },
-{ path: '**', pathMatch: 'full', redirectTo: '' }
+{ path: 'Ingresa', component: IngresaComponent },
+{ path: 'Cuenta', component: CuentaComponent },
+{ path: 'Home', component: HomeComponent },
+{ path: '**', pathMatch: 'full', redirectTo: 'ingresa' }
 ];
 
-export const app_routing= RouterModule.forRoot (APP_ROUTES);
+export const APP_ROUTING = RouterModule.forRoot (APP_ROUTES, { useHash : true });
